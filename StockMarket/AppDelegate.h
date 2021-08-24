259604599a78ddd2093ef4a8798311dd85797d3f
actionsljx <actionsljx@foxmail.com>
2021-08-24 22:05:12 +0800

    Initial Commit

---
+++ StockMarket/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StockMarket
+//
+//  Created by 李家先 on 2021/8/24.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentCloudKitContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
